16aee887bc18959dd7a444d004eb569a26483bab
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-04-22 20:46:29 +0530

    added the logic to generate the qr code

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import qrcode
+from io import BytesIO
+from base64 import b64encode
 
 app = Flask(__name__)
 
@@ -8,6 +10,19 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+
+def generateQR():
+    memory = BytesIO()
+    data = request.form.get("link")
+    img = qrcode.make(data)
+    img.save(memory, format="PNG")
+    memory.seek(0)
+    
+    base64_img = "data:image/png;base64," + \
+        b64encode(memory.getvalue()).decode("ascii")
+
+    return render_template("index.html", data=base64_img)
 
 if __name__ == '__main__':
     app.run(debug=True)
